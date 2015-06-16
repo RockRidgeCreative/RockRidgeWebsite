849658179d6f77a5f6d5ba7169d6e2406babe8ef
KeepCalmandCodeOn <brian@rockridgecreative.com>
2015-06-16 16:35:08 -0500

    Commit of initial working files to repo
    Added al working files to repo

---
+++ js/animations.js
@@ -0,0 +1,12 @@
+/***********************************************
+ JQuery function to toggle the navigation menu
+***********************************************/
+
+$(function() { 
+    $('.menu-button').on('click', function(){
+        $('.off-canvas-menu').toggleClass('open');
+        $('.menu-bar1').toggleClass('active');
+        $('.menu-bar2').toggleClass('active');
+        $('.menu-bar3').toggleClass('active');
+    });
+});
